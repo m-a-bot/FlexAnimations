d4ad7c115f5c3bc18ed0f387d9b207e79b7c8eff
Iznagi1 <kasbievazamat@gmail.com>
2023-03-12 23:48:17 +0300

    Меин должен использоваться только для конструторов классов. Теперь у нас 2 виртуальных окружения и реквайраментов, так как для линукса и винды есть отличия в либах

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ from settings import *
 from views.Container import ContainerView
 from views.test import TestView
 
-def main():
-    window = arcade.Window(WIDTH, HEIGHT, TITLE, resizable=False)
+from GUI import GUI
 
-    container_view = ContainerView()
-    window.show_view(container_view)
-    arcade.run()
+
+# def main():
+#     window = arcade.Window(WIDTH, HEIGHT, TITLE, resizable=False)
+#
+#     container_view = ContainerView()
+#     window.show_view(container_view)
+#     arcade.run()
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    window = arcade.Window(1920, 1080, title="GUI", fullscreen = False)
+    window.show_view(GUI(window))
+    arcade.run()
+
