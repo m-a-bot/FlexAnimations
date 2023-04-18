8824d965ff6714ec44c0cba434e0a071b51cd8b5
GrigorySkvortsov <g.skvorcov102@gmail.com>
2023-04-18 13:27:46 +0300

    add wave animation

--- animations/wave.py
+++ animations/wave.py
@@ -4,11 +4,14 @@ from settings import *
 from functools import partial
 import math
 
+
 def move_cart_to_center(x, y):
     return x - WIDTH // 2, y - HEIGHT // 2
 
+
 def move_cart_to_origin(x, y):
     return x + WIDTH // 2, y + HEIGHT // 2
+
 
 def polar_to_cart(r, phi):
     x = r * math.cos(phi)
@@ -16,11 +19,13 @@ def polar_to_cart(r, phi):
 
     return x, y
 
+
 def cart_to_polar(x, y):
     phi = math.atan2(y, x)
     r = math.sqrt(x ** 2 + y ** 2)
 
     return r, phi
+
 
 def increase_angle(x, y, inc):
     x, y = move_cart_to_center(x, y)
@@ -30,32 +35,41 @@ def increase_angle(x, y, inc):
     x, y = move_cart_to_origin(x, y)
     return x, y
 
+
+def move(x, y, inc, a, b, c, d):
+
+    x, y = x + inc, a + b * math.sin(c * x + d)
+
+    return x, y
+
+
 class Wave(Animation):
     def __init__(self):
         super().__init__()
-        self.speed = 10
+        self.speed = 40
         self.center_x = WIDTH // 2
         self.center_y = HEIGHT // 4
         self.center = (self.center_x, self.center_y)
+
+        self.a = 360
+        self.b = 100
+        self.c = 0.05
+        self.d = - WIDTH / 2
+        self.sprite_width = 100
+
     def fill_sprites(self, sprites):
         super().fill_sprites(sprites)
         sprites.append(arcade.Sprite(filename=":resources:gui_basic_assets/red_button_press.png",
                                      center_x=self.center_x,
                                      center_y=self.center_y,
-                                     scale=1)
-                      )
+                                     scale=1))
 
     def update_sprite(self, delta_time, sprite):
-        sprite.center_x, sprite.center_y = increase_angle(sprite.center_x, sprite.center_y, delta_time * self.speed)
-
+        if sprite.center_x > WIDTH + self.sprite_width:
+            sprite.center_x = - self.sprite_width
+        sprite.center_x, sprite.center_y = move(sprite.center_x, sprite.center_y,
+                                                delta_time * self.speed, self.a, self.b, self.c, self.d)
 
     def animation_run(self, sprites, delta_time):
-        # print(1)
-        # map(partial(self.update_sprite, delta_time), sprites)
-
         for sprite in sprites:
             self.update_sprite(delta_time, sprite)
-
-        pass
-
-
