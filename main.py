9713d7aee5f41d5852a1e32aeac5fb867970a4ed
siberianstrength <115467121+siberianstrength@users.noreply.github.com>
2023-04-19 21:08:56 +0300

    добавил кнопку appply для выбора анимации и фигуры.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from GUI import GUI
 
 if __name__ == "__main__":
    
-    window = arcade.Window(1920, 1080, TITLE, update_rate=1/FPS)
+    window = arcade.Window(WIDTH, HEIGHT, TITLE, update_rate=1/FPS)
 
     window.show_view(GUI())
 
